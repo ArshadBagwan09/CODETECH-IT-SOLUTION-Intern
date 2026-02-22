d9d6562c1e2c1509e19080f015c70c9b70fda43f
Hallo Arshad <bagwanarshad95@gmail.com>
2026-02-22 23:33:29 +0530

    Code  is Modified okk

--- Task-1/FILE_HANDLING_UTILITY.java
+++ Task-1/FILE_HANDLING_UTILITY.java
@@ -1,3 +1,26 @@
+import java.io.*;
+import java.rmi.server.SocketSecurityException;
+import java.util.Scanner;
+
 public class FILE_HANDLING_UTILITY {
-  
+
+  private static final String file_name = "CodeTech It Solution .txt";
+
+  public static void createfile() {
+    try {
+      File file = new File(file_name);
+      if (file.createNewFile()) {
+        System.err.println("File is create Successfully: " + file_name);
+      } else {
+        System.err.println("File is Alredy created");
+      }
+    } catch (IOException e) {
+      System.err.println("Error While File creation");
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+
+  }
 }
